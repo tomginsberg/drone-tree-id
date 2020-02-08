5317961dab28575de792336d9c38dff51673d46b
Ro-ee Tal <roeetal1@gmail.com>
2020-02-07 18:13:52 -0800

    rename depth encoder state dicts

--- deepent/config.py
+++ deepent/config.py
@@ -17,7 +17,7 @@ def add_deepent_config(cfg):
 
     _C.MODEL.DEPTH_ENCODER.STEM_OUT_CHANNELS = 64
     _C.MODEL.DEPTH_ENCODER.FREEZE_AT = 0
-    _C.MODEL.DEPTH_ENCODER.OUT_FEATURES = ["res2", "res3", "res4", "res5"]
+    _C.MODEL.DEPTH_ENCODER.OUT_FEATURES = ["d_res2", "d_res3", "d_res4", "d_res5"]
     _C.MODEL.DEPTH_ENCODER.RES2_OUT_CHANNELS = 256
 
     if not torch.cuda.is_available():
--- deepent/modelling/backbone/depth_encoder.py
+++ deepent/modelling/backbone/depth_encoder.py
@@ -1,6 +1,86 @@
-from lib.detectron2.detectron2.layers import FrozenBatchNorm2d
-from lib.detectron2.detectron2.modeling.backbone.resnet import BottleneckBlock, ResNet, make_stage, BasicStem
-from detectron2.modeling import BACKBONE_REGISTRY
+from torch import nn
+import numpy as np
+
+from detectron2.modeling import Backbone
+from detectron2.layers import FrozenBatchNorm2d, ShapeSpec
+from detectron2.modeling.backbone.resnet import BottleneckBlock, ResNet, make_stage, BasicStem, ResNetBlockBase
+
+
+class DepthEncoder(Backbone):
+    def __init__(self, stem, stages, num_classes=None, out_features=None):
+        """
+        Args:
+            stem (nn.Module): a stem module
+            stages (list[list[ResNetBlock]]): several (typically 4) stages,
+                each contains multiple :class:`ResNetBlockBase`.
+            num_classes (None or int): if None, will not perform classification.
+            out_features (list[str]): name of the layers whose outputs should
+                be returned in forward. Can be anything in "dstem", "dlinear", or "dres2" ...
+                If None, will return the output of the last layer.
+        """
+        super(DepthEncoder, self).__init__()
+        self.stem = stem
+        self.num_classes = num_classes
+
+        current_stride = self.stem.stride
+        self._out_feature_strides = {"d_stem": current_stride}
+        self._out_feature_channels = {"d_stem": self.stem.out_channels}
+
+        self.stages_and_names = []
+        for i, blocks in enumerate(stages):
+            for block in blocks:
+                assert isinstance(block, ResNetBlockBase), block
+                curr_channels = block.out_channels
+            stage = nn.Sequential(*blocks)
+            name = "d_res" + str(i + 2)
+            self.add_module(name, stage)
+            self.stages_and_names.append((stage, name))
+            self._out_feature_strides[name] = current_stride = int(
+                current_stride * np.prod([k.stride for k in blocks])
+            )
+            self._out_feature_channels[name] = blocks[-1].out_channels
+
+        if num_classes is not None:
+            self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+            self.linear = nn.Linear(curr_channels, num_classes)
+
+            # Sec 5.1 in "Accurate, Large Minibatch SGD: Training ImageNet in 1 Hour":
+            # "The 1000-way fully-connected layer is initialized by
+            # drawing weights from a zero-mean Gaussian with standard deviation of 0.01."
+            nn.init.normal_(self.linear.weight, std=0.01)
+            name = "d_linear"
+
+        if out_features is None:
+            out_features = [name]
+        self._out_features = out_features
+        assert len(self._out_features)
+        children = [x[0] for x in self.named_children()]
+        for out_feature in self._out_features:
+            assert out_feature in children, "Available children: {}".format(", ".join(children))
+
+    def forward(self, x):
+        outputs = {}
+        x = self.stem(x)
+        if "d_stem" in self._out_features:
+            outputs["d_stem"] = x
+        for stage, name in self.stages_and_names:
+            x = stage(x)
+            if name in self._out_features:
+                outputs[name] = x
+        if self.num_classes is not None:
+            x = self.avgpool(x)
+            x = self.linear(x)
+            if "d_linear" in self._out_features:
+                outputs["d_linear"] = x
+        return outputs
+
+    def output_shape(self):
+        return {
+            name: ShapeSpec(
+                channels=self._out_feature_channels[name], stride=self._out_feature_strides[name]
+            )
+            for name in self._out_features
+        }
 
 def build_depth_encoder_backbone(cfg, input_shape):
     """
@@ -24,15 +104,15 @@ def build_depth_encoder_backbone(cfg, input_shape):
         stem = FrozenBatchNorm2d.convert_frozen_batchnorm(stem)
 
     # fmt: off
-    out_features        = cfg.MODEL.DEPTH_ENCODER.OUT_FEATURES
-    depth               = cfg.MODEL.RESNETS.DEPTH
-    num_groups          = cfg.MODEL.RESNETS.NUM_GROUPS
-    width_per_group     = cfg.MODEL.RESNETS.WIDTH_PER_GROUP
+    out_features = cfg.MODEL.DEPTH_ENCODER.OUT_FEATURES
+    depth = cfg.MODEL.RESNETS.DEPTH
+    num_groups = cfg.MODEL.RESNETS.NUM_GROUPS
+    width_per_group = cfg.MODEL.RESNETS.WIDTH_PER_GROUP
     bottleneck_channels = num_groups * width_per_group
-    in_channels         = cfg.MODEL.DEPTH_ENCODER.STEM_OUT_CHANNELS
-    out_channels        = cfg.MODEL.DEPTH_ENCODER.RES2_OUT_CHANNELS
-    stride_in_1x1       = cfg.MODEL.RESNETS.STRIDE_IN_1X1
-    res5_dilation       = cfg.MODEL.RESNETS.RES5_DILATION
+    in_channels = cfg.MODEL.DEPTH_ENCODER.STEM_OUT_CHANNELS
+    out_channels = cfg.MODEL.DEPTH_ENCODER.RES2_OUT_CHANNELS
+    stride_in_1x1 = cfg.MODEL.RESNETS.STRIDE_IN_1X1
+    res5_dilation = cfg.MODEL.RESNETS.RES5_DILATION
     # fmt: on
     assert res5_dilation in {1, 2}, "res5_dilation cannot be {}.".format(res5_dilation)
 
@@ -42,7 +122,7 @@ def build_depth_encoder_backbone(cfg, input_shape):
 
     # Avoid creating variables without gradients
     # It consumes extra memory and may cause allreduce to fail
-    out_stage_idx = [{"res2": 2, "res3": 3, "res4": 4, "res5": 5}[f] for f in out_features]
+    out_stage_idx = [{"d_res2": 2, "d_res3": 3, "d_res4": 4, "d_res5": 5}[f] for f in out_features]
     max_stage_idx = max(out_stage_idx)
     for idx, stage_idx in enumerate(range(2, max_stage_idx + 1)):
         dilation = res5_dilation if stage_idx == 5 else 1
@@ -60,4 +140,4 @@ def build_depth_encoder_backbone(cfg, input_shape):
             for block in blocks:
                 block.freeze()
         stages.append(blocks)
-    return ResNet(stem, stages, out_features=out_features)
+    return DepthEncoder(stem, stages, out_features=out_features)
