030eff5389056a212c6807bf7a64e710837c1c62
tomginsberg <tom@beit.tech>
2020-02-10 18:15:44 -0800

    legacy logging script

---
+++ tools/legacy_logger.py
@@ -0,0 +1,20 @@
+import json
+import os
+from sys import argv
+
+import wandb
+
+if __name__ == '__main__':
+    metrics = os.path.join(argv[1], 'metrics.json')
+
+    with open(metrics, 'r') as f:
+        raw_data = f.read()
+
+    wandb.init(project='forest')
+
+    for line in f.split('\n')[:-1]:
+        obj = json.loads(line)
+        itr = obj['iteration']
+        for k, v in obj.items():
+            if k != 'iteration':
+                wandb.log({k, v}, step=itr)
