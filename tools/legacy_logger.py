b19fdbaee18cb2ab292e1ec2c88e10eae172b4f1
tomginsberg <tom@beit.tech>
2020-02-10 18:38:51 -0800

    logger for images

--- tools/legacy_logger.py
+++ tools/legacy_logger.py
@@ -1,20 +1,37 @@
+import argparse
 import json
 import os
-from sys import argv
+from glob import glob
 
+import cv2
 import wandb
 
+parser = argparse.ArgumentParser(description='Log some data')
+parser.add_argument('--type', help='images or metrics')
+parser.add_argument('--dest', help='location of output folder')
+
+args = parser.parse_args()
+
 if __name__ == '__main__':
-    metrics = os.path.join(argv[1], 'metrics.json')
+    if args.type == 'images':
+        path = os.path.join(args.dest, 'vis', '*')
+        images = glob(path)
+        for image in path:
+            wandb.log({os.path.basename(image): wandb.Image(cv2.imread(image)[:, :, ::-1])})
 
-    with open(metrics, 'r') as f:
-        raw_data = f.read()
+    elif args.type == 'metrics':
+        metrics = os.path.join(args.dest, 'metrics.json')
 
-    wandb.init(project='forest')
+        with open(metrics, 'r') as f:
+            raw_data = f.read()
 
-    for line in raw_data.split('\n')[:-1]:
-        obj = json.loads(line)
-        itr = obj['iteration']
-        for k, v in obj.items():
-            if k != 'iteration':
-                wandb.log({k, v}, step=itr)
+        wandb.init(project='forest')
+
+        for line in raw_data.split('\n')[:-1]:
+            obj = json.loads(line)
+            itr = obj['iteration']
+            for k, v in obj.items():
+                if k != 'iteration':
+                    wandb.log({k: v}, step=itr)
+    else:
+        print('Please put metric or image')
