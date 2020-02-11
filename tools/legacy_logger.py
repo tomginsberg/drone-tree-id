5b7414032300d2150e9bdde725fd196012605266
tomginsberg <tom@beit.tech>
2020-02-11 08:22:12 -0800

    updated train script to turn off wandb

--- tools/legacy_logger.py
+++ tools/legacy_logger.py
@@ -5,6 +5,7 @@ from glob import glob
 
 import cv2
 import wandb
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description='Log some data')
 parser.add_argument('--type', help='images or metrics')
@@ -16,9 +17,14 @@ if __name__ == '__main__':
     wandb.init(project='forest')
 
     if args.type == 'images':
-        path = os.path.join(args.dest, 'vis', '**', '*')
+        if 'vis' in [os.path.basename(x) for x in glob(os.path.join(args.dest, '*'))]:
+            path = os.path.join(args.dest, 'vis', '**', '*.png')
+        else:
+            path = os.path.join(args.dest, '*.png')
+        assert len(path) > 0, f'No images found from {args.dest}'
         images = glob(path)
-        for image in images:
+        print(f'Logging {len(path)} images')
+        for image in tqdm(images):
             wandb.log({'/'.join(image.split('/')[-2:]): wandb.Image(cv2.imread(image)[:, :, ::-1])})
 
     elif args.type == 'metrics':
@@ -27,11 +33,12 @@ if __name__ == '__main__':
         with open(metrics, 'r') as f:
             raw_data = f.read()
 
-        for line in raw_data.split('\n')[:-1]:
+        print(f'Logging metrics for {args.dest}')
+        for line in tqdm(raw_data.split('\n')[:-1]):
             obj = json.loads(line)
             itr = obj['iteration']
             for k, v in obj.items():
                 if k != 'iteration':
                     wandb.log({k: v}, step=itr)
     else:
-        print('Please put metrics or images')
+        print('Please use --dest={metrics, images}')
--- tools/train_net.py
+++ tools/train_net.py
@@ -1,4 +1,6 @@
+import logging
 import os
+from collections import OrderedDict
 
 import wandb
 
@@ -8,7 +10,8 @@ from deepent.data.register_datasets import register_datasets
 from detectron2.checkpoint import DetectionCheckpointer
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup, launch
-from detectron2.evaluation import COCOEvaluator, DatasetEvaluators, verify_results
+from detectron2.evaluation import COCOEvaluator, DatasetEvaluators, verify_results, inference_on_dataset, \
+    print_csv_format, DatasetEvaluator
 from detectron2.utils.events import CommonMetricPrinter, JSONWriter
 from detectron2.utils.logger import setup_logger
 from tools.wandb_writer import WandbWriter
@@ -22,14 +25,70 @@ class Trainer(DefaultTrainer):
         return DatasetEvaluators(evaluators)
 
     def build_writers(self):
-        if args.eval_only:
-            return [CommonMetricPrinter(self.max_iter),
-                    JSONWriter(os.path.join(self.cfg.OUTPUT_DIR, "metrics.json"))
-                    ]
         return [CommonMetricPrinter(self.max_iter),
                 JSONWriter(os.path.join(self.cfg.OUTPUT_DIR, "metrics.json")),
                 WandbWriter()
                 ]
+
+    @classmethod
+    def test(cls, cfg, model, evaluators=None):
+        """
+        Args:
+            cfg (CfgNode):
+            model (nn.Module):
+            evaluators (list[DatasetEvaluator] or None): if None, will call
+                :meth:`build_evaluator`. Otherwise, must have the same length as
+                `cfg.DATASETS.TEST`.
+
+        Returns:
+            dict: a dict of result metrics
+        """
+        logger = logging.getLogger(__name__)
+        if isinstance(evaluators, DatasetEvaluator):
+            evaluators = [evaluators]
+        if evaluators is not None:
+            assert len(cfg.DATASETS.TEST) == len(evaluators), "{} != {}".format(
+                len(cfg.DATASETS.TEST), len(evaluators)
+            )
+
+        results = OrderedDict()
+        for idx, dataset_name in enumerate(cfg.DATASETS.TEST):
+            # for f in glob.glob(os.path.join('output', 'inference', '*')):
+            #             #     print(f'Removing {f}')
+            #             #     os.remove(f)
+            data_loader = cls.build_test_loader(cfg, dataset_name)
+            # When evaluators are passed in as arguments,
+            # implicitly assume that evaluators can be created before data_loader.
+            if evaluators is not None:
+                evaluator = evaluators[idx]
+            else:
+                try:
+                    evaluator = cls.build_evaluator(cfg, dataset_name)
+                except NotImplementedError:
+                    logger.warn(
+                        "No evaluator found. Use `DefaultTrainer.test(evaluators=)`, "
+                        "or implement its `build_evaluator` method."
+                    )
+                    results[dataset_name] = {}
+                    continue
+            results_i = inference_on_dataset(model, data_loader, evaluator)
+            results[dataset_name] = results_i
+            if comm.is_main_process():
+                assert isinstance(
+                    results_i, dict
+                ), "Evaluator must return a dict on the main process. Got {} instead.".format(
+                    results_i
+                )
+                logger.info("Evaluation results for {} in csv format:".format(dataset_name))
+                print_csv_format(results_i)
+                logger = logging.getLogger(__name__)
+                for task, res in results.items():
+                    # Don't print "AP-category" metrics since they are usually not tracked.
+                    wandb.log({f'{task}/{k}': v for k, v in res.items() if "-" not in k})
+
+        if len(results) == 1:
+            results = list(results.values())[0]
+        return results
 
 
 def setup(args):
@@ -45,7 +104,6 @@ def setup(args):
 
 def main(args):
     cfg = setup(args)
-
     if args.eval_only:
         model = Trainer.build_model(cfg)
         DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
@@ -68,8 +126,8 @@ if __name__ == "__main__":
         register_datasets('RGBD-Tree-Segs')
 
     args = default_argument_parser().parse_args()
-
-    if not args.eval_only:
-        wandb.init(project="forest")
+    if args.eval_only:
+        wandb.init(project="forest", name=args.opts[-2])
+    wandb.init(project="forest")
 
     launch(main, args.num_gpus, args.num_machines, args.machine_rank, args.dist_url, args=(args,))
