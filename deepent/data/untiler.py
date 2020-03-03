f491fd6a249d049e411368dffaeaca82138cc07f
Ro-ee Tal <roeetal1@gmail.com>
2020-03-03 09:49:52 -0800

    untiler scale

--- deepent/data/untiler.py
+++ deepent/data/untiler.py
@@ -12,12 +12,10 @@ class Untiler:
         self.output = output
         self._tree_id = 0
         self._path_to_tiles = path_to_tiles
-        self._tile_height = 0
-        self._tile_width = 0
 
     def __call__(self, *args, **kwargs):
 
-        tiles = glob(path_to_tiles, "*.png")
+        tiles = glob(self._path_to_tiles, "*.png")
 
         with shapefile.Writer(self.output) as shp:
             shp.shapeType = 5  # set shapetype to polygons
@@ -28,13 +26,28 @@ class Untiler:
         for tile in tiles:
             img = cv2.imread(tile)
             width, height = img.shape[1], img.shape[0]
-            predictions = predictor(img)
+            # TODO: implement
+            x_scale, y_scale, x_shift, y_shift = load_transform(tile)
+            predictions = self._predictor(img)
+            predictions = predictions["instances"].to("cpu")
             if predictions.has("pred_masks"):
                 for (polygon, area, cls) in _format_predictions(predictions, height, width):
-                    # TODO: rescale polygon by tile positioning and shapefile geometry
-                    shp.poly(polygon)
-                    shp.record(self._tree_id, area, cls)
+                    shp.poly(shift_polygon(polygon, x_scale, y_scale, x_shift, y_shift))
+                    # TODO: is class the id number or the string????????
+                    shp.record(self._tree_id, area * x_scale * y_scale, cls)
                     self._tree_id += 1
+
+
+def shift_polygon(polygon, x_scale, y_scale, x_shift, y_shift):
+    """
+    x and y scale -> should be inverse of scaling used to tile.
+    shift in shapefile coordinates
+    """
+    rescale_x = lambda x: (x * x_scale + x_shift)
+    rescale_y = lambda y: (y * y_scale + y_shift)
+    # TODO: How to deal with nested polygons(i.e. holes)?
+    rescaled_poly = [[rescale_x(x), rescale_y(y)] for x, y in polygon]
+    return rescaled_poly
 
 
 def format_predictions(predictions, height, width):
