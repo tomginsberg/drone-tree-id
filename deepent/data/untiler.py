5b136b097450024a159bb2677559030340548d9b
Ro-ee Tal <roeetal1@gmail.com>
2020-03-03 11:28:37 -0800

    untiler

--- deepent/data/untiler.py
+++ deepent/data/untiler.py
@@ -32,20 +32,20 @@ class Untiler:
             predictions = predictions["instances"].to("cpu")
             if predictions.has("pred_masks"):
                 for (polygon, area, cls) in _format_predictions(predictions, height, width):
-                    shp.poly(shift_polygon(polygon, x_scale, y_scale, x_shift, y_shift))
+                    shp.poly(affine_polygon(polygon, x_scale, y_scale, x_shift, y_shift))
                     # TODO: is class the id number or the string????????
                     shp.record(self._tree_id, area * x_scale * y_scale, cls)
                     self._tree_id += 1
 
 
-def shift_polygon(polygon, x_scale, y_scale, x_shift, y_shift):
+def affine_polygon(polygon, x_scale, y_scale, x_shift, y_shift):
     """
     x and y scale -> should be inverse of scaling used to tile.
     shift in shapefile coordinates
     """
     rescale_x = lambda x: (x * x_scale + x_shift)
     rescale_y = lambda y: (y * y_scale + y_shift)
-    # TODO: How to deal with nested polygons(i.e. holes)?
+    # TODO: deal with nested polygons(i.e. holes)?
     rescaled_poly = [[rescale_x(x), rescale_y(y)] for x, y in polygon]
     return rescaled_poly
 
