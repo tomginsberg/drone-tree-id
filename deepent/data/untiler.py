e48d13b82e2b272fd026356242d6fd2162c2329f
tomginsberg <tom@beit.tech>
2020-03-07 18:35:59 -0800

    secondary predcitor

--- deepent/data/untiler.py
+++ deepent/data/untiler.py
@@ -18,30 +18,67 @@ class PolygonRecord:
         self.polygons[tile_num].append(polygon)
         self.meta[tile_num].append((id_, area, cls))
 
-    def get_neighbours(self, tile_num):
-        if tile_num == 0:
-            # First tile, ie no neighbours
-            return []
-        if tile_num < self.x_tiles:
-            # First row, neighbours are only the tiles one left
-            return self.polygons[tile_num - 1]
-        if tile_num > self.x_tiles:
-            # Past first row
-            if tile_num % self.x_tiles == 0:
-                # First col only neighbour is above
-                return self.polygons[tile_num - self.x_tiles]
+    def get_neighbours(self, tile_num, lookahead=False):
+        xw = self.x_tiles
+        # left, top, top left,
+        # top right, bottom, right, bottom right, bottom left
+        behind, infront = [tile_num - 1, tile_num - xw, tile_num - xw - 1], [tile_num - xw + 1, tile_num + xw,
+                                                                             tile_num + 1,
+                                                                             tile_num + 1 + xw, tile_num - 1 + xw]
+        if lookahead:
+            if tile_num % xw == 0:
+                # left border, take above and all in front but bottom left
+                indices = [behind[1]] + infront[:-1]
+            elif tile_num + 1 % xw == 0:
+                # right border, take all behind and bottom + bottom left
+                indices = behind + [infront[1], infront[-1]]
             else:
-                # Non trivial case, need to check all three neighbours
-                return self.polygons[tile_num - 1] + self.polygons[tile_num - self.x_tiles] + self.polygons[
-                    tile_num - self.x_tiles - 1]
+                indices = behind + infront
+        else:
+            # No lookahead
+            # If on the left border only take above
+            if tile_num % xw == 0:
+                indices = [behind[1]]
+            else:
+                indices = behind
+
+        return self._get_many_polygons(indices)
+
+    # if tile_num == 0:
+    #     # First tile, ie no neighbours
+    #     if lookahead
+    #         return []
+    # if tile_num < self.x_tiles:
+    #     # First row, neighbours are only the tiles one left
+    #     if lookahead:
+    #         return self.polygons[tile_num - 1] + self.polygons[tile_num + 1] + self.polygons[
+    #             tile_num + self.x_tiles - 1] + self.polygons[tile_num + self.x_tiles + 1]
+    #     return self.polygons[tile_num - 1]
+    # if tile_num > self.x_tiles:
+    #     # Past first row
+    #     if tile_num % self.x_tiles == 0:
+    #         # First col only neighbour is above
+    #         return self.polygons[tile_num - self.x_tiles]
+    #     else:
+    #         # Non trivial case, need to check all three neighbours
+    #         return self.polygons[tile_num - 1] + self.polygons[tile_num - self.x_tiles] + self.polygons[
+    #             tile_num - self.x_tiles - 1]
 
     def poly_meta(self):
         return zip(self.polygons, self.meta)
 
+    def _get_many_polygons(self, list_of_indices):
+        polys = []
+        for idx in list_of_indices:
+            if 0 <= idx < len(self.polygons):
+                polys += self.polygons[idx]
+        return polys
+
 
 class Untiler:
-    def __init__(self, predictor):
-        self._predictor = predictor
+    def __init__(self, primary_predictor, secondary_predictor=None):
+        self.primary_predictor = primary_predictor
+        self.secondary_predictor = secondary_predictor
 
     def __call__(self, path_to_tiles: str, output: str, epsg_ref="32610", *args, **kwargs):
         tree_id = 0
@@ -51,7 +88,7 @@ class Untiler:
         x_scale, y_scale = offsets['transform']
 
         tiles = sorted(glob(os.path.join(path_to_tiles, "*.png")),
-                     key=lambda x: int(os.path.basename(x).split('_')[-1][:-4]))
+                       key=lambda x: int(os.path.basename(x).split('_')[-1][:-4]))
         poly_record = PolygonRecord(num_tiles=len(tiles), x_tiles=offsets['x_tiles'])
         removed_polys, total_polys = 0, 0
 
@@ -59,9 +96,9 @@ class Untiler:
             img = cv2.imread(tile)
             width, height = img.shape[1], img.shape[0]
             x_shift, y_shift = offsets[os.path.realpath(tile)]
-            predictions = self._predictor(img)
+            predictions = self.primary_predictor(img)
             predictions = predictions["instances"].to("cpu")
-            neighbours = poly_record.get_neighbours(tile_num + 200)
+            neighbours = poly_record.get_neighbours(tile_num + 200, lookahead=False)
 
             if predictions.has("pred_masks"):
                 for (polygon, area, cls) in format_predictions(predictions, height, width):
@@ -70,12 +107,37 @@ class Untiler:
                         # IPython.embed()
                         # print(affine_polygon(polygon, x_scale, y_scale, x_shift, y_shift), len(polygon))
                         next_poly = Polygon(affine_polygon(polygon, x_scale, y_scale, x_shift, y_shift)).simplify(0.1)
-                        if new_polygon_q(next_poly, neighbours, iou_thresh=.90, area_thresh=3):
+                        if new_polygon_q(next_poly, neighbours, iou_thresh=.70, area_thresh=3):
                             poly_record.put(tile_num + 200, next_poly, tree_id,
                                             area * x_scale * y_scale, cls)
                             tree_id += 1
                         else:
                             removed_polys += 1
+
+        if self.secondary_predictor is not None:
+            for tile_num, tile in tqdm(enumerate(tiles[200:300])):
+                img = cv2.imread(tile)
+                width, height = img.shape[1], img.shape[0]
+                x_shift, y_shift = offsets[os.path.realpath(tile)]
+                predictions = self.secondary_predictor(img)
+                predictions = predictions["instances"].to("cpu")
+                neighbours = poly_record.get_neighbours(tile_num + 200, lookahead=True)
+
+                if predictions.has("pred_masks"):
+                    for (polygon, area, cls) in format_predictions(predictions, height, width):
+                        total_polys += 1
+                        if len(polygon) > 4:
+                            # IPython.embed()
+                            # print(affine_polygon(polygon, x_scale, y_scale, x_shift, y_shift), len(polygon))
+                            next_poly = Polygon(affine_polygon(polygon, x_scale, y_scale, x_shift, y_shift)).simplify(
+                                0.1)
+                            if new_polygon_q(next_poly, neighbours, iou_thresh=.70, area_thresh=3):
+                                poly_record.put(tile_num + 200, next_poly, tree_id,
+                                                area * x_scale * y_scale, cls)
+                                tree_id += 1
+                            else:
+                                removed_polys += 1
+
         print(f'Inference done. {removed_polys}/{total_polys} polygons removed')
         with shapefile.Writer(output) as shp:
             shp.shapeType = 5  # set shape type to polygons
@@ -191,7 +253,40 @@ if __name__ == '__main__':
 
     predictor = DefaultPredictor(cfg)
 
-    ut = Untiler(predictor)
+    config_file = 'configs/deepent_fuse_rcnn_R_50_FPN.yaml'
+    threshold = 0.5
+    model = 'output/baseline_fuse_07_02_2020/model_0089999.pth'
+    samples = 1
+    type_ = 'many'
+    opts = []
+
+
+    class Args:
+        def __init__(self, conf, tr, mod, sam, ty, opt):
+            self.config_file = conf
+            self.threshold = tr
+            self.model = mod
+            self.samples = sam
+            self.type = ty
+            self.opts = []
+            self.output = None
+
+
+    args = Args(config_file, threshold, model, samples, type_, opts)
+
+    cfg = get_cfg()
+    add_deepent_config(cfg)
+    cfg.merge_from_file(config_file)
+    cfg.merge_from_list(opts)
+    cfg.MODEL.WEIGHTS = model
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = threshold
+    cfg.freeze()
+    default_setup(cfg, args)
+    from tools.predictor import RGBDPredictor
+
+    secondary_predictor = RGBDPredictor(cfg)
+
+    ut = Untiler(predictor, secondary_predictor)
 
     ut(path_to_tiles='/home/ubuntu/CPTA-nInferenceTiles/tiles/CPT2a-n',
        output='/home/ubuntu/drone-tree-id/output/shapefiles/rgbnonduplicate/cpta-n')
